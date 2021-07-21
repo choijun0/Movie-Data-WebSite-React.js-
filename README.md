b183822cc3fd72a2b9d2cdcd5b7f54d7b8491c63
choi2 <>
2021-07-21 10:20:23 +0000

    <Fetching Data Mechanism with axios module>
    to fetch data from TheMovieDataBase choose axios module. to fetch i used axios api and get method. inside the axios instance baseURL,params are, baseURL + params's properties + additional URL handed by get method and optional object used for Search and append_to_response, get method put together this URL and send request to data sever. + package.json.lock is deleted because it exist with yarn.lock file it means npm is used, this means two package manager is using this is not good so delete it and by using yarn upgrade(similiar with npm update) adjusted dependencies(modules)

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Learning React and ES6 by building a Movie Discovery App.
 - [ ] Detail
 
 ## API Verbs
-- [ ] Now playing (Moive)
-- [ ] Upcoming (TV, Movie)  
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
-- [ ] Airing Today (TV)
+- [X] Now playing (Moive)
+- [X] Upcoming (Movie)  
+- [X] Top Rated (TV)
+- [X] Popular (TV, Movie)
+- [X] Airing Today (TV)
+- [X] TV Show Detail
+- [X] Movie Detail
+- [X] Search (Movie, TV)
--- src/api.js
+++ src/api.js
@@ -9,7 +9,34 @@ const api = axios.create({
 	}
 });
 
-api.get("tv/popular"); //fetch의미 baseURL + tv/popular + params 
-//request data from url
+export const movieApi = {
+	nowPlaying: () => api.get("movie/now_playing"),
+	upcoming: () => api.get("movie/upcoming"),
+	popular: () => api.get("movie/popular"),
+	movieDetail : id => api.get(`movie/${id}`, {
+		params : {
+			append_to_response : "videos"  
+		}
+	}),
+	search : term => api.get("search/movie",{
+		params : {
+			query : encodeURIComponent(term)
+		}
+	})
+}
 
-export default api;
+export const tvApi = {
+	topRated: () => api.get("tv/top_rated"),
+	popular: () => api.get("tv/popular"),
+	airingToday: () => api.get("tv/airing_today"),
+	showDetail : id => api.get(`tv/${id}`, {
+		params : {
+			append_to_response : "videos"  
+		}
+	}),
+	search : term => api.get("search/tv",{
+		params : {
+			query : encodeURIComponent(term) //특수문자 or 공백 등을 URI화 하면 깨진다. 그래서 이걸로 바꿔줌
+		}
+	})
+}
